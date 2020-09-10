c4d7f8ffb897254d788a61b22a6b5f28e96793c2
Heli Sivunen <heli@mooncat.tv>
2020-09-10 13:21:14 +0100

    Note list renders on page, yey

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
+import NoteList from "./NoteList.js";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <NoteList />
       </div>
     );
   }
